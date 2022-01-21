453a7969c9990277acd4dc9321e5f5986b3d19e7
uglyfence <76257046+tac0coder@users.noreply.github.com>
2022-01-21 17:43:20 +0000

    just puch it

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function makePlaceHolderImage(width,height,id){
-  document.getElementById(id).outerHTML = '<img src=via.placeholder.com/'+width.toString()+'x'+height.toString()+' </img>';
+  document.getElementById(id).outerHTML = '<img src=https://via.placeholder.com/'+width.toString()+'x'+height.toString()+' </img>';
 }
-makePlaceHolderImage(window.innerWidth,window.innerHeight,'1');
+makePlaceHolderImage(window.innerWidth,300,'1');

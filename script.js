6d5ed6d3c05b13a56e7b46c0ee24de0b199344d1
uglyfence <76257046+tac0coder@users.noreply.github.com>
2022-01-21 17:36:53 +0000

    kjn3ieodjp;2qd

---
+++ script.js
@@ -0,0 +1,4 @@
+function makePlaceHolderImage(width,hieght,id){
+  document.getElementById(id).outerHTML = '<img src=via.placeholder.com/'+width.toString()+'x'+height.toString()+' </img>';
+}
+makePlaceHolderImage(window.innerWidth,window.innerHeight);
